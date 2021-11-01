3ebd426cc193275ba08824ad93eb53ae1c58bc6a
jacobbbyy <aliefalieman@gmail.com>
2021-11-01 19:22:48 +0800

    updated

--- src/components/pages/SocialWork.js
+++ src/components/pages/SocialWork.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../../App.css';
+import Border from '../Border';
+import HeroSection from '../HeroSection';
+import './SocialWork.css';
 
 function SocialWork() {
     return (
         <>
-           <div className='socialwork'> </div>   
+           <div className='socialwork'>
+           <Border>
+            
+            </Border>
+              </div>   
         </>
     );
 }

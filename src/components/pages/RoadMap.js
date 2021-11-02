570b933fb4db1df1ce1bfa40e847382e4446ca8a
jacobbbyy <aliefalieman@gmail.com>
2021-11-02 23:21:42 +0800

    updateddddddddddddddddddddddddddd

--- src/components/pages/RoadMap.js
+++ src/components/pages/RoadMap.js
@@ -10,7 +10,53 @@ import Footer from '../Footer';
 function RoadMap() {
     return (
         <>
-            <div className='roadmap'> 
+            <div className='roadmap'>   
+               <div className='phase--container'>
+              <div className='header-roadmap'>
+              <h1>ROADMAP:</h1>
+              <p>10% royalties will be imposed to the sale of Genji Nft, where 30% of the weekly royalties will 
+                 be accumulated for the reinvestment to further develop Genji Nft utility. 50% of the weekly 
+                 royalties will be distributed to the community wallet and holders will decide on what to do 
+                 with the funds. 20% of the weekly royalties will be given to the Genji Dev team.</p>
+                 <p>Holders of Genji Nft will enjoy early access to the next nft collections.</p>
+                 <p> Click for more info</p>
+                 
+                  <div className='pre--phase'>
+                   <h1>PRE-PHASE (Q3 2021)</h1>
+                   <p>-Roadmap planning and artwork designs</p>
+                   <p>-Dev team setting up discord, website and details on Genji</p>
+                  </div>
+
+                  <div className='phase--1'>
+                  <h1>PHASE 1 (Q4 2021)</h1>
+                  <p>-Open discord server to public and raise project presence</p>
+                  <p>-Partnerships with solana projects and communities</p>
+                  <p>-Sneak peaks of artwork and asking for community suggestions</p>
+                  <p>-Implementing community suggestions on the project</p>
+                  <p>-Mint day + secondary market listing</p>
+                  </div>
+
+                  <div className='phase--2'>
+                  <h1>PHASE 2 (Q1 2022)</h1>
+                   <p>-Start up the recruitment for Solana Help Fund members
+                    (community vote & voluntary involvement of solana nft influencers)</p>
+                   <p>-Begin with the setting up of Genji DAO</p>
+                   <p>-Start working on Genji Merch</p>
+                   <p>-Mysterious Airdrop to holders (more details later)</p>
+                   <p>-Ongoing partnerships with community voted solana projects and communities</p>
+                  </div>
+                  
+                  <div className='phase--3'>
+                  <h1>PHASE 3 (Q2 2022)</h1>
+                   <p>-First Solana Help Fund collaboration event </p>
+                   <p>-Release of Genji Merch and sales of merch (50% of merch profit goes to Genji DAO)</p>
+                   <p>-Increase future utility for Genji Nft (to be decided by Genji Dao)</p>
+                   <p>-Official launch of Genji Nft online store</p>
+                  </div>
+
+              </div>
+
+            </div>
             <div className='discords-item'>
              <Discord/>
              </div> 
--- src/components/pages/SocialWork.js
+++ src/components/pages/SocialWork.js
@@ -10,19 +10,21 @@ function SocialWork() {
         <>
            <div className='socialwork'>  
              <div className='page2--content'>
-               <h1 class="hero--heading">SOCIAL WORK:</h1>
-               <p class="hero--description">Here at Genji we adore changes and hope to make a difference by changing how NFT’s
+               <h1 >SOCIAL WORK:</h1>
+               <div className="hero--description">
+              <p>Here at Genji we adore changes and hope to make a difference by changing how NFT’s
                benefit everyone including the non-NFT investors. Nft is what made us connect with each
                other here in this community and we would like to unite as ONE to make a difference in this
                world. By participating in the mint event of our nft, you are not just getting a masterpiece
                (which every trait is hand-drawn and electronically rendered to perfection), you will be
                funding the start up of SOL HELP FUNDS (SHF), a dream that we here at Genji’s want to
-               achieve. 
-
-              SHF is just a concept for now but every successful story starts with a concept. SHF aims to
+               achieve.</p> 
+              <p>SHF is just a concept for now but every successful story starts with a concept. SHF aims to
               be the first NFT help fund that will assist in reducing hardships or disasters faced by the
               people. With the presence of a help fund, NFT projects in the solana blockchain will be able
               to pool up funds that were allocated to help the world and make a bigger difference together </p>
+             </div>
+               
 
              </div>
              <div className='discords---item'>
